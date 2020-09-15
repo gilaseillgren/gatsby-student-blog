c2e6cee198b9c4e3982a7d58d948b593bf932d92
Elias Gillgren <33657449+gilaseillgren@users.noreply.github.com>
2020-09-15 12:38:22 +0200

    changed style and theme color

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -4,13 +4,13 @@ import { CardWrapper, P, H1 } from "../elements"
 export const ContentCard = ( {date, title, excerpt, slug}) => {
     return (
       <CardWrapper>
-        <P size="xSmall">
+        <P size="xSmall" textAlign="center" margin="0 0 0.5rem 0" color="dark2">
             {date}
         </P>
-        <H1>
+        <H1 textAlign="center" margin="0 0 1rem 0">
             {title}
         </H1>
-        <P size="xSmall">
+        <P size="small" textAlign="center" margin="0 0 1.5rem 0" color="dark2">
             {excerpt}
         </P>
       </CardWrapper>
